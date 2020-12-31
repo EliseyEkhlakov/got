9e09f30f961c7a7460515e532a2409b742d1b3f3
Елисей Ехлаков <elisey.ekhlakov@krit.pro>
2020-12-31 16:30:20 +0400

    lesson 100

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,11 +3,19 @@ import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ErrorMessage from '../errorMessage';
-import CharacterPage from '../characterPage';
+import CharacterPage from '../pages/characterPage';
+import BooksPage from '../pages/booksPage';
+import HousesPage from '../pages/housesPage';
+import ItemList from '../itemList';
+import ItemDetails from '../itemDetails';
+import gotService from '../../services/gotService';
 
 import './app.css';
 
 export default class App extends Component {
+
+    gotService = new gotService();
+
     state = {
         showRandomChar: true,
         error: false
@@ -50,6 +58,33 @@ export default class App extends Component {
                         </Col>
                     </Row>
                     <CharacterPage/>
+                    <BooksPage/>
+                    <HousesPage/>                    
+                    {/* <Row>
+                        <Col md='6'>
+                            <ItemList 
+                                onItemSelected={this.onItemSelected}
+                                getData={this.gotService.getAllBooks}
+                                renderItem={(item) => item.name}
+                                />
+                        </Col>
+                        <Col md='6'>
+                            <ItemDetails charId={this.state.selectedChar}/>
+                        </Col>
+                    </Row> */}
+
+                    {/* <Row>
+                        <Col md='6'>
+                            <ItemList 
+                                onItemSelected={this.onItemSelected}
+                                getData={this.gotService.getAllHouses}
+                                renderItem={(item) => item.name}
+                                />
+                        </Col>
+                        <Col md='6'>
+                            <ItemDetails itemId={this.state.selectedChar}/>
+                        </Col>
+                    </Row> */}
                 </Container>
             </>
         );
